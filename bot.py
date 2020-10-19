566691afef79526ea38100a9c817a00b75ad3522
Samrid-Pandit <64601348+Samrid-Pandit@users.noreply.github.com>
2020-10-19 22:07:40 +0545

    Both Adding And removing roles works;  but removing role is very time and resource intensive; people can use give me admin any time so i need to add cooldonw, and it always tries to remove role from every people, i need to make it fetch only the members with MODS role and remove the rolles of people who aren't applicable to get the role. Too much if/ else is used so i need to make it more efficient. But other than performance issues everything works nice!

--- bot.py
+++ bot.py
@@ -6,16 +6,28 @@ from discord.utils import get
 import datetime
 import random
 
+intents = discord.Intents.default()
+intents.members = True
+
+#Class For actual Bot management
+#Variables
+token = "NzY3Mjc5MjAzMjY3Mzc5Mjgw.X4vmcQ.uobt0bqwcyVkBvNWw1753qfV6WE"
+
+#Declaring Prefix
+client = commands.Bot(command_prefix=".", case_insensitive=True, intents=intents)
+
+
 #Class to get the list of applicable members
 class applicable_members:
 
     #Variables
-    member={}
     people = []
     role_name = "Mods"
     bots=[767279203267379280 ,270904126974590976, 751026843687321660]
+    member={}
 
     def api(people, mee6API, member):
+
         #Gets the data from api
         result = requests.get(mee6API).json()
 
@@ -29,85 +41,115 @@ class applicable_members:
         #List of user-id of people illegible to get Mod role
         people = list(member.values())[:3]
 
-        #Returns people
-        return (people)
+        #Returns
+        return(people, member)
 
-#Class For actual Bot management
+
 class samrid_bot:
-    #Variables
-    token = "NzY3Mjc5MjAzMjY3Mzc5Mjgw.X4vmcQ.HWfMnd0Yv-qxAw8QMV_7U115exI"
-    
-    #Declaring Prefix
-    client = commands.Bot(command_prefix=".", case_insensitive=True)
 
-    @client.event
-    async def on_ready():
-        print("Bot is ready")
+	@client.event
+	async def on_ready():
+		print("Bot is ready")
+				
+	#Response to be given on message
+	@client.listen('on_message')
+	async def no_prefix(message):
 
-    #Response to be given on message
-    @client.listen('on_message')
-    async def no_prefix(message):
 
-        #mee6API
-        mee6API = "https://mee6.xyz/api/plugins/levels/leaderboard/"+str(message.guild.id)+"?limit=999&page=0"
 
-        #Checks If the messager isn't bot
-        if message.author.id in applicable_members.bots:
-            pass
-        else:
-            try:
-                #Checks if the message is for making admin changes
-                if "give me mod" in message.content:
+		#mee6API
+		mee6API = "https://mee6.xyz/api/plugins/levels/leaderboard/"+str(message.guild.id)+"?limit=999&page=0"
 
-                    #Need to Run the command after getting the id 
-                    applicable_members.api(applicable_members.people, mee6API, applicable_members.member)
+		#Checks If the messager isn't bot
+		if message.author.id in applicable_members.bots:
+			pass
+		else:
+			
+				#Checks if the message is for making admin changes
+				if "give me mod" in message.content:
 
-                
-                    #Variables
-                    people = applicable_members.api(applicable_members.people, mee6API, applicable_members.member)
-                    person = message.author.id
-                    prsn = message.author
-                    role_name = applicable_members.role_name
+					#Need to Run the command after getting the id 
+					func = applicable_members.api(applicable_members.people, mee6API, applicable_members.member)
 
-                    #Checks if the person if applicable to get the role        
-                    if str(person) in people:
-                        role = discord.utils.get(message.guild.roles, name=role_name)
-                        await prsn.add_roles(role)
-                        print(str(prsn) + " Has Been given Mod Role")
-                        await message.channel.send(message.author.mention + " Has been given mod role!")
-                    else:
-                        await message.channel.send("Get into the top 3 first motherfucker! "+message.author.mention)
-                
-                else:
-                    #RESPONSES TO SHITTY THINGS
-                    #Response to imagine
-                    if "imagine" == message.content.split()[0]:
-                        await message.channel.send("I can't even " + message.content +", bro!")
+				
+					#Variables
+					people = func[0]
+					person = message.author.id
+					prsn = message.author
+					role_name = applicable_members.role_name
+					peoples =[]
 
-                    #Response to f
-                    elif "f" == message.content.lower():
-                        await message.channel.send("f")
-                    
-                    #Response to no u
-                    elif "no u" == message.content.lower():
-                        await message.channel.send("no u")
-                        
-            except Exception:
-                pass
+					# role_member = client.role_name.members
+					for user in people:
+						users = await client.fetch_user(user)
+						peoples.append(users)
+					
+					#Checks if the person if applicable to get the role        
+					if str(person) in people:
+						role = discord.utils.get(message.guild.roles, name=role_name)
+						await prsn.add_roles(role)
+						print(str(prsn) + " Has Been given Mod Role")
+						await message.channel.send(message.author.mention + " Has been given mod role!")
+					else:
+						await message.channel.send("Get into the top 3 first motherfucker! "+message.author.mention)
 
-    
-    @client.command()
-    async def hello(ctx,*, input):
-        await ctx.send("Fuck you " + input)
-    
-    @client.command()
-    async def waifu(ctx,input=None):
-        waifu = str(random.randrange(1,10))
-        if input == None:
-            await ctx.send(ctx.author.mention + " You are " + waifu + "/10 waifu!")
-        else:
-            await ctx.send(input + " is " + waifu + "/10 waifu!")
-    
+					#remove the roles of people who aren't applicable
+					for member in list(client.get_all_members()):
+						if member in peoples:
+							pass
+						else:
+							role = discord.utils.get(message.guild.roles, name=role_name)
+							await member.remove_roles(role)
 
-    #Runs the bot after compiling everything
-    client.run(token)
+				#RESPONSES TO SHITTY THINGS
+				else:
+					#Response to imagine
+					try:
+						if "imagine" == message.content.split()[0]:
+							await message.channel.send("I can't even " + message.content +", bro!")
+
+						#Response to f
+						elif "f" == message.content.lower():
+							await message.channel.send("f")
+						
+						#Response to no u
+						elif "no u" == message.content.lower():
+							await message.channel.send("no u")
+
+					except Exception:
+						pass
+
+			
+			
+
+	
+	@client.command()
+	async def hello(ctx,*, input = None):
+		if input == None:
+			await ctx.send("Fuck you! " + ctx.author.mention)
+		else:
+			await ctx.send("Fuck you! " + input)
+	
+	@client.command()
+	async def waifu(ctx,input=None):
+		waifu = str(random.randrange(1,10))
+		if input == None:
+			await ctx.send(ctx.author.mention + " You are " + waifu + "/10 waifu!")
+		else:
+			await ctx.send(input + " is " + waifu + "/10 waifu!")
+	
+	@client.command()
+	async def tata(ctx,input=None):
+		if input == None:
+			await ctx.send(ctx.author.mention + " How alone can you be to not even find a person in discord for making him/her suck your dick virtually?")
+		else:
+			await ctx.send(input + " Suck " + ctx.author.mention + "'s Dick!")
+	
+	@client.command()
+	async def gg(ctx, input=None):
+		if input == None:
+			await ctx.send("Good Game Well Played")
+		else:
+			await ctx.send("Good Game Well Played "+input)
+
+	client.run(token)
