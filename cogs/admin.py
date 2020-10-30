5edc72ef91422531d8b41c8bd4396f5fc3a23b92
Samrid-Pandit <64601348+Samrid-Pandit@users.noreply.github.com>
2020-10-31 01:38:15 +0545

    Added some admin commands :)

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 from datetime import datetime, timedelta
+import asyncio
 
 class admin(commands.Cog):
 	def __init__(self, bot):
@@ -17,7 +18,6 @@ class admin(commands.Cog):
 		"""
 		Variables for the EMBED DOWN
 		"""
-		embed = discord.Embed(title = f"{user.name}#{user.discriminator}", description = user.mention, colour = discord.Color.blue())
 		mentions = [role.mention for role in user.roles]
 		roles = mentions[1:]
 		no_of_roles = len(roles)
@@ -72,6 +72,7 @@ class admin(commands.Cog):
 		"""
 		Converting the list into a string
 		"""
+		roles = roles[::-1]
 		roles = ' '.join(roles)
 		permission = ', '.join(permission)
 		acknowledgement = ', '.join(acknowledgement)
@@ -80,6 +81,8 @@ class admin(commands.Cog):
 		"""
 		Main Embed
 		"""
+		embed = discord.Embed(description = user.mention, colour = discord.Color.blue())
+		embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
 		embed.add_field(name= "**Joined**", value = joined_at  , inline = True)
 		embed.add_field(name= "**Registered**", value = created_at  , inline = True)
 		embed.add_field(name= f"**Roles**[{no_of_roles}]", value = roles  , inline = False)
@@ -89,5 +92,119 @@ class admin(commands.Cog):
 		embed.set_footer(text = f"ID: {user.id} • RIGHT NOW!")
 		await ctx.send(embed=embed)
 
+	@commands.command(aliases = ['av', 'pp'])
+	async def avatar(self, ctx, user: discord.Member = None):
+		if not user:
+			user = ctx.author
+
+		embed = discord.Embed(title = 'Avatar', colour = discord.Color.blue())
+		embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+		embed.set_image(url=user.avatar_url)
+		embed.set_footer(text = f"Requested by {user}")
+		
+		await ctx.send(embed=embed)
+	
+	
+	@commands.command(aliases=['m'])
+	@commands.has_permissions(manage_roles = True)
+	async def mute(self, ctx, user:discord.Member = None, time:int = None):
+		if not user:
+			await ctx.send("Please specify whom to mute")
+		else:
+			role = discord.utils.get(ctx.guild.roles, name="Muted")
+			if not role:
+				perms = discord.Permissions(send_messages=False, read_messages=True)
+				await ctx.guild.create_role(name="Muted", permissions = perms)
+				role = discord.utils.get(ctx.guild.roles, name="Muted")
+			await user.add_roles(role)
+
+			if not time:
+				embed = discord.Embed(description = "Has been muted!" )
+				embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+				embed.set_footer(text=f'Requested by {ctx.author}')
+				await ctx.send(embed=embed, delete_after=5)
+			else:
+				embed1 = discord.Embed(description = f"Has been muted for {time} minutes!" )
+				embed1.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+				embed1.set_footer(text=f'Requested by {ctx.author}')
+				await ctx.send(embed=embed1, delete_after=5)
+
+				if role in user.roles:
+					await asyncio.sleep(time*60)
+					await user.remove_roles(role)
+					embed2 = discord.Embed(description = f"Has been unmuted after {time} minutes!" )
+					embed2.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+					embed2.set_footer(text=f'Requested by {ctx.author}')
+					await ctx.send(embed=embed2, delete_after=5)
+			
+		await asyncio.sleep(4)
+		Message = ctx.message
+		await Message.delete()
+				
+
+	@commands.command(aliases=['um'])
+	@commands.has_permissions(manage_roles = True)
+	async def unmute(self, ctx, user:discord.Member = None):
+		role = discord.utils.get(ctx.guild.roles, name="Muted")
+		if not user:
+			await ctx.send("Please mention whom to unmute!")
+		else:
+			if role in user.roles:
+				await user.remove_roles(role)
+				embed = discord.Embed(description = "Has been unmuted!" )
+				embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+				embed.set_footer(text=f'Requested by {ctx.author}')
+				await ctx.send(embed=embed, delete_after=5)
+			else:
+				embed = discord.Embed(description = "Hasn't been muted yet!" )
+				embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+				embed.set_footer(text=f'Requested by {ctx.author}')
+				await ctx.send(embed=embed, delete_after=5)
+		
+		await asyncio.sleep(4)
+		Message = ctx.message
+		await Message.delete()
+				
+
+	@commands.command(aliases=['k'])
+	@has_permissions(kick_members = True)
+	async def kick(self, ctx, user:discord.Member = None,*, reason = None):
+		if not user:
+			await ctx.send("Please specify whom to kick!")
+		else:
+			await user.kick(reason=reason)
+			embed = discord.Embed(description = f"Has been Kicked, because {reason}")
+			embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+			embed.set_footer(text=f'Requested by {ctx.author}')
+			await ctx.send(embed=embed)
+	
+	@commands.command(aliases=['b'])
+	@commands.has_permissions(ban_members=True)
+	async def ban(self, ctx, user: discord.Member, *, reason=None):
+		await user.ban(reason=reason)
+		embed = discord.Embed(description = f"Has been Banned, because {reason}")
+		embed.set_author(name= user, url=user.avatar_url, icon_url=user.avatar_url)
+		embed.set_footer(text=f'Requested by {ctx.author}')
+		await ctx.send(embed=embed)
+
+	# The below code unbans player.
+	@commands.command(aliases=['ub'])
+	@commands.has_permissions(administrator=True)
+	async def unban(self, ctx, *, member):
+		banned_users = await ctx.guild.bans()
+		try:
+			member_name, member_discriminator = member.split("#")
+		except Exception:
+			await ctx.send("Use command properly! eg: `.unban MEE6#4876`")
+
+		for ban_entry in banned_users:
+			user = ban_entry.user
+
+			if (user.name, user.discriminator) == (member_name, member_discriminator):
+				await ctx.guild.unban(user)
+				await ctx.send(f'Unbanned {user.mention}')
+
+
+			
 def setup(bot):
 	bot.add_cog(admin(bot))
