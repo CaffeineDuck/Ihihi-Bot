ccb81c43f432de97c470629154559560ac64b927
Samrid-Pandit <64601348+Samrid-Pandit@users.noreply.github.com>
2020-11-08 15:56:11 +0545

    Removed the reddit links storer (in json)

--- cogs/background.py
+++ cogs/background.py
@@ -2,28 +2,14 @@ import discord
 from discord.ext import commands, tasks
 import os
 import json
-import praw
+from asyncpraw import Reddit
 from itertools import cycle
-from utils.ThisPersonDoesNotExist import Person
+from thispersondoesnotexist import get_online_person, save_picture
 """
 Prints "-------------------------"
 """
 def star():
 	print("-----------------------------------------------")
-
-'''
-Credentials for reddit praw authentication!
-'''
-reddit = praw.Reddit(client_id = os.environ['ID'],
-				client_secret = os.environ['SECRET'],
-				username = os.environ['REDDIT_USERNAME'],
-				password = os.environ['PASSWORD'],
-				user_agent = "Ihihihi")
-			
-
-subreddits = ['memes', 'cursedcomments', 'aww', 'hentai','ass', 'boobs', 'cumsluts', 'pussy', 'RealGirls', 'porngifs', 'creampie', 'creampiegifs']
-linked = []
-status = cycle(['Your THICC Ass', 'Gay Porn', '-_-', 'When you will die!'])
 
 
 class Background(commands.Cog):
@@ -39,43 +25,18 @@ class Background(commands.Cog):
 		star()
 		print("Image Grabber has started!")
 		star()
-		for x in range(100):
-			person = Person(fetch_online=True)
-			person.save(f"./Other/Images/Random-Person/{x}.jpeg")
+		for x in range(1,100):
+			picture = await get_online_person()
+			await save_picture(picture, f"./Other/Images/Random-Person/{x}.jpeg")
 		star()
 		print("Image Grabber has completed!")
 		star()
-	
-	"""
-	Reddit grabber uses saves the links of from various subreddits to a json file
-	"""
-	@staticmethod
-	@tasks.loop(hours = 5)
-	async def reddit_grabber():
-		star()
-		print("Reddit Grabber Started")
-		star()
-		data = {}
-		for subrd in subreddits:
-			subreddit = reddit.subreddit(subrd)
-			data[subrd] = [] 
-			hot = subreddit.hot(limit=100)
-			for submission in hot:
-				data[subrd].append({
-					'Title': submission.title,
-					'Link': submission.url
-				})
-		with open('./Other/json/links.json', 'w') as file:
-			json.dump(data, file)
-		star()
-		print("Reddit grabber has compeleted its task!")
-		star()
+
 
 """
 Starts the loop of fetching the data from reddit
 """
 Background.image_grabber.start()
-Background.reddit_grabber.start()
 
 def setup(bot):
 	bot.add_cog(Background(bot))
