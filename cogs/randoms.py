851c2704bae3faa517576b07d0afa09f5c09138e
Samrid-Pandit <64601348+Samrid-Pandit@users.noreply.github.com>
2020-11-08 15:57:41 +0545

    Used cached_reddit module to cache the links of various images fetched from reddit!

--- cogs/randoms.py
+++ cogs/randoms.py
@@ -1,80 +1,82 @@
 import discord
 from discord.ext import commands, tasks
-import random
+
 import requests
 import aiohttp
+import random
 import os
-import json
-import praw
+import time
+import asyncpraw
 
-'''
-Credentials for reddit praw authentication!
-'''
-reddit = praw.Reddit(client_id = os.environ['ID'],
-				client_secret = os.environ['SECRET'],
-				username = os.environ['REDDIT_USERNAME'],
-				password = os.environ['PASSWORD'],
-				user_agent = "Ihihihi")
-
-"""
-Reddit sender sends the embed by reading the links in the json!
-"""
-async def reddit_sender(self, subrd, ctx):
-	with open('./Other/json/links.json') as json_file:
-		main = []
-		data = json.load(json_file)
-		sub = data[subrd]
-		for data_list in sub:
-			main.append([data_list['Title'], data_list['Link']])
-
-	datas = random.choice(main)
-	link = datas[1]	
-	embed = discord.Embed(title = datas[0],timestamp = ctx.message.created_at, colour = discord.Colour.blue())
-	embed.set_image(url = link)
-	embed.set_footer(text=f"Requested by {ctx.author}!")
-	if ".jpg" in str(link) or '.png' in str(link) or ".gif" in str(link[-4:-1]):
-		await ctx.send(embed=embed)
-	elif ".gifv" in str(link):
-		await ctx.send(link)
-	else:
-		await reddit_sender(self, subrd, ctx)
-	return
-
+from utils.Personal.cached_reddit import RedditPostCacher
+ 
+reddit = asyncpraw.Reddit(client_id = os.environ['ID'],
+                    client_secret = os.environ['SECRET'],
+                    user_agent = 'Ihihi Bot')
 
 class Random(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		
+		self.subreddits = ('meme', 'aww', 'cursedcomments')
+		self.cache = RedditPostCacher(self.subreddits, './Other/cache/Randoms.pickle')
+		self.cache.cache_posts.start()
+	
+	async def _reddit_sender(self, ctx, subrd: str, title: str):
+		"""Fetches from reddit and sends results
+
+			Parameters
+			----------
+			ctx : discord.ext.commands.Context
+					The invocation context
+			subrd : str
+					The subreddit to fetch from
+			title : str
+					The title to use in the embed
+		"""
+		# Gets the data from reddit!
+		submission = await self.cache.get_random_post(subrd)
+
+		# Sends the embed!
+		embed = discord.Embed(title=title, timestamp=ctx.message.created_at, color = discord.Colour.gold())
+		embed.set_image(url=submission)
+		embed.set_footer(text=f'Requested by {ctx.author}')
+		await ctx.send(embed=embed)
 
 	@commands.command(aliases = ['memes'])
 	@commands.cooldown(1, 1, commands.BucketType.user)
 	async def meme(self, ctx):
-		sub = "memes"
-		await reddit_sender(self, sub, ctx)
+		await self._reddit_sender(ctx, "meme", "Memes for you <3")
 	
 	@commands.command(aliases = ['cursedcomment', 'cursedcomments'])
 	@commands.cooldown(1, 1, commands.BucketType.user)
 	async def cursed(self, ctx):
-		sub = "cursedcomments"
-		await reddit_sender(self, sub, ctx)
-
-	@commands.command(aliases = ['r'])
-	@commands.cooldown(1, 1, commands.BucketType.user)
-	async def reddit(self, ctx, sub = "memes"):
-		subreddit = reddit.subreddit(sub)
-		all_subs = [] 
-		hot = subreddit.top(limit=50)
-		for submission in hot:
-			all_subs.append(submission)
-		link = random.choice(all_subs)
-		embed = discord.Embed(title = link.title)
-		embed.set_image(url = link.url)
-		await ctx.send(embed=embed)
+		await self._reddit_sender(ctx, "cursedcomments", "This isn't cursed its you!")
 
 	@commands.command(aliases = ['cute'])
 	@commands.cooldown(1, 1, commands.BucketType.user)
 	async def aww(self, ctx):
-		sub = "aww"
-		await reddit_sender(self, sub, ctx)
+		await self._reddit_sender(ctx, "aww", "This is cute but YOU AREN'T!")
+
+	@commands.command(aliases = ['reddit','subreddit'])
+	async def r(self, ctx, subred = 'memes'):
+		try:#checks for subreddit
+			subreddit = await reddit.subreddit(subred)
+			all_subs = []
+	
+			async for submissions in subreddit.top(limit=50):
+				all_subs.append(submissions)
+		
+			random_subs = random.choice(all_subs)
+			name = random_subs.title
+			url = random_subs.url
+	
+			embed = discord.Embed(title = name)
+			embed.set_image(url = url)
+			embed.set_footer(text = f'Requested by: {ctx.author}')
+			await ctx.send(embed = embed)
+		except:#if not sends this
+			await ctx.send("Subreddit not found. Try again")
 
 	@commands.command(aliases = ['puppy', 'pup'])
 	@commands.cooldown(1, 1, commands.BucketType.user)
@@ -88,7 +90,7 @@ class Random(commands.Cog):
 		embed.set_footer(text="https://dog.ceo/")
 		await ctx.send(embed=embed)
 	
-	@commands.command(aliases = ['foxy'])
+	@commands.command()
 	@commands.cooldown(1, 1, commands.BucketType.user)
 	async def fox(self, ctx):
 		async with aiohttp.ClientSession() as cs:
@@ -99,6 +101,7 @@ class Random(commands.Cog):
 		embed.set_image(url=image)
 		embed.set_footer(text="https://randomfox.ca")
 		await ctx.send(embed=embed)
+
 	
 	@commands.command(aliases=['doesnotexist', 'fakeperson'])
 	async def thispersondoesnotexist(self, ctx):
@@ -108,6 +111,6 @@ class Random(commands.Cog):
 		embed.set_image(url=f"attachment://{_image}")
 		embed.set_footer(text=f'Requested by {ctx.author}')
 		await ctx.send(file=file, embed=embed)
-
+	
 def setup(bot):
 	bot.add_cog(Random(bot))
